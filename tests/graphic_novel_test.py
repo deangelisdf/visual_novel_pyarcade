e28ad83cbe4056f6c877c41e74e1311b385f12b3
De Angelis Domenico Francesco <deangelis1993@gmail.com>
2023-09-19 17:15:02 +0200

    ignore unimplemented test

--- tests/graphic_novel_test.py
+++ tests/graphic_novel_test.py
@@ -7,7 +7,7 @@ import unittest
 src_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, src_path)
 
-import graphic_novel # noqa: E402
+#import graphic_novel # noqa: E402
 
 class gn_testing(unittest.TestCase):
     def test_setup(self):
