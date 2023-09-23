be0f5517d2e4260eb0ab950c902ae7cb90cf8e8b
De Angelis Domenico Francesco <deangelis1993@gmail.com>
2023-09-23 16:31:40 +0200

    note

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ Unit Tests
 from unittest import mock
 
 def get_window_dummy(w: int, h: int):
+    # inspired from pyglet
     # A named function instead of a lambda for clarity in debugger views.
     def _get_dummy_shader_program(*args, **kwargs):
         return mock.MagicMock(width=w, height=h)
